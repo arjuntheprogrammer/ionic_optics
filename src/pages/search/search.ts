f57dcf6c59e0504b31e0d971793323a1dfafa6a1
Arjun Gupta <saimzn4241@gmail.com>
2017-07-10 15:01:26 +0530

    search

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -2,16 +2,35 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {RedditService} from '../../app/services/reddit.service';
 import {RedditsPage} from '../reddits/reddits';
+import {Data} from '../../app/services/data.service';
 
 
 @Component({
   selector: 'search',
   templateUrl: 'search.html',
-
+  providers: [Data],
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController) {
+  searchTerm: string = '';
+  items: any;
+
+  constructor(public navCtrl: NavController, public dataService: Data) {
 
   }
+  ionViewDidLoad() {
+    this.setFilteredItems();
+  }
+  setFilteredItems() {
+    if (this.searchTerm != ""){
+        this.items = this.dataService.filterItems(this.searchTerm);
+    }
+    else{
+      this.items=null;
+    }
+
+  }
+
+
+
 }
